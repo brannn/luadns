34b7825758394030a5676cea92513ec288ec5dda
Brandon Huey <brandon@stylograph.net>
2014-06-29 16:05:21 -0700

    CNAMEs

--- polytap.com.lua
+++ polytap.com.lua
@@ -5,12 +5,14 @@ fred_addr = "23.92.53.85"
 
 a(_a, fred_addr, ttl)
 
---  http://www.tumblr.com/docs/ja/custom_domains
-cname("www", "fred.polytap.com", ttl)
-
 -- A records
 a("fred", fred_addr, ttl)
  
+-- CNAME records
+cname("www", "fred.polytap.com", ttl)
+cname("cdb", "global.prod.fastly.net", ttl)
+cname("api", "global.prod.fastly.net", ttl)
+
 -- MX records
 mx(_a, "mx-1.rightbox.com", 10)
 mx(_a, "mx-2.rightbox.com", 10)
