3011815c1d8bba9dea8aa5b79d46fb5771035332
Brandon Huey <brandon@stylograph.net>
2014-06-29 16:38:42 -0700

    Add SSHFP

--- polytap.com.lua
+++ polytap.com.lua
@@ -17,3 +17,7 @@ cname("api", "global.prod.fastly.net", ttl)
 mx(_a, "mx-1.rightbox.com", 10)
 mx(_a, "mx-2.rightbox.com", 10)
 mx(_a, "mx-3.rightbox.com", 10)
+
+-- SSHFP records
+sshfp("fred", 1, "5b41710c8ffe9ba704f50a434df320746ee0f0f7")
+sshfp("fred", 2, "ddb69f56419c0c02c8d89867d7965b9251f29c85")
