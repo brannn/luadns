455a9e7ac95468bd1fc576bc9092b739fa3c1342
Brandon Huey <brandon@stylograph.net>
2014-06-29 16:02:40 -0700

    Pobox MX

--- polytap.com.lua
+++ polytap.com.lua
@@ -8,5 +8,10 @@ a(_a, fred_addr, ttl)
 --  http://www.tumblr.com/docs/ja/custom_domains
 cname("www", "fred.polytap.com", ttl)
 
--- fred 
+-- A records
 a("fred", fred_addr, ttl)
+ 
+-- MX records
+mx(_a, "mx-1.rightbox.com", 10)
+mx(_a, "mx-2.rightbox.com", 10)
+mx(_a, "mx-3.rightbox.com", 10)
