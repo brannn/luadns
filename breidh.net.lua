ff7987e07fce5d6ef7eebee5f9113861157c80a5
Brandon Huey <brandon@stylograph.net>
2014-06-30 20:17:08 -0700

    more zones and typo fix

--- breidh.net.lua
+++ breidh.net.lua
@@ -1,4 +1,4 @@
--- stylograph.net
+-- breidh.net
 -- _a = mydomain.com
 ttl = 300
 fred_addr = "192.241.254.112"
@@ -9,7 +9,7 @@ a(_a, fred_addr, ttl)
 a("fred", fred_addr, ttl)
  
 -- CNAME records
-cname("www", "fred.stylograph.net", ttl)
+cname("www", "fred.breidh.net", ttl)
 cname("cdb", "global.prod.fastly.net", ttl)
 cname("api", "global.prod.fastly.net", ttl)
 
@@ -19,5 +19,5 @@ mx(_a, "mx-2.rightbox.com", 10)
 mx(_a, "mx-3.rightbox.com", 10)
 
 -- SSHFP records
-sshfp("fred.stylograph.net", 1, "5b41710c8ffe9ba704f50a434df320746ee0f0f7")
-sshfp("fred.stylograph.net", 2, "ddb69f56419c0c02c8d89867d7965b9251f29c85")
+sshfp("fred.breidh.net", 1, "5b41710c8ffe9ba704f50a434df320746ee0f0f7")
+sshfp("fred.breidh.net", 2, "ddb69f56419c0c02c8d89867d7965b9251f29c85")
