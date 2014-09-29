b3e1d01211d296a3051832b400cf36835273312c
Brandon Huey <brandon@stylograph.net>
2014-09-29 10:38:32 -0700

    Change host

--- polytap.com.lua
+++ polytap.com.lua
@@ -1,17 +1,18 @@
 -- polytap.com
 -- _a = mydomain.com
 ttl = 300
-fred_addr = "166.78.254.137"
+juno_addr = "166.78.254.137"
 
-a(_a, fred_addr, ttl)
+a(_a, juno_addr, ttl)
 
 -- A records
-a("fred", fred_addr, ttl)
+a("juno", juno_addr, ttl)
  
 -- CNAME records
-cname("www", "fred.polytap.com", ttl)
+cname("www", "juno.polytap.com", ttl)
 cname("cdb", "global.prod.fastly.net", ttl)
 cname("api", "global.prod.fastly.net", ttl)
+cname("fred", "juno.polytap.com", ttl)
 
 -- MX records
 mx(_a, "mx-1.rightbox.com", 10)
@@ -19,5 +20,5 @@ mx(_a, "mx-2.rightbox.com", 10)
 mx(_a, "mx-3.rightbox.com", 10)
 
 -- SSHFP records
-sshfp("fred.polytap.com", 1, "5b41710c8ffe9ba704f50a434df320746ee0f0f7")
-sshfp("fred.polytap.com", 2, "ddb69f56419c0c02c8d89867d7965b9251f29c85")
+sshfp("juno.polytap.com", 1, "5b41710c8ffe9ba704f50a434df320746ee0f0f7")
+sshfp("juno.polytap.com", 2, "ddb69f56419c0c02c8d89867d7965b9251f29c85")
